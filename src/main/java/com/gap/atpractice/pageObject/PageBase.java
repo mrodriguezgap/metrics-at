f09f0efcb13e7807009863929ab7d6aad6246046
Manuel Rodriguez <mrodriguez@growthaccelerationpartners.com>
2017-05-25 17:20:25 -0600

    Extended PageBase and SeleniumBase

---
+++ src/main/java/com/gap/atpractice/pageObject/PageBase.java
@@ -0,0 +1,7 @@
+package com.gap.atpractice.pageObject;
+
+/**
+ * Created by auto on 25/05/17.
+ */
+public class PageBase {
+}
---
+++ src/main/java/com/gap/atpractice/testSuites/LoginTestNG.java
@@ -0,0 +1,7 @@
+package com.gap.atpractice.testSuites;
+
+/**
+ * Created by auto on 22/05/17.
+ */
+public class LoginTestNG {
+}
